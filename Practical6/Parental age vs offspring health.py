7805e92a47ca8dbb5abb437821bbdd232d41fa89
mingshi1 <mingshi.21@intl.zju.edu.cn>
2022-03-23 19:09:02 +0800

    Parental age vs offspring health.py(2)

--- Practical6/Parental age vs offspring health.py
+++ Practical6/Parental age vs offspring health.py
@@ -11,6 +11,9 @@ my_dict={}
 # To find y corresponding with x, the list should be changed into dictionary
 # As I don't know the formula to do this, I type by my hands.
 a={30:1.03, 35:1.07, 40:1.11, 45:1.17, 50:1.23, 55:1.32, 60:1.42, 65:1.55, 70:1.72, 75:1.94}
-# type the value in x into a[ ] below to get the y
-print(a[40])
+# Then I got the command "dict(zip())" from my friend Zhu, HengYu. By typing zip(a,b), a is a list, b is a list. It will
+# generate a dictionary with the least length of the list. zip() connects a and b, dict() change it into a dictionary.
+b=dict(zip(x,y))
+# type the value in x into b[ ] below to get the y
+print(b[40])
 
