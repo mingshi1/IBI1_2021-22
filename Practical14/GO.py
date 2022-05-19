99179aeb4e7fca5f1d0029fdc73393060a57da71
mingshi1 <mingshi.21@intl.zju.edu.cn>
2022-05-19 20:29:39 +0800

    Practical14

---
+++ Practical14/GO.py
@@ -0,0 +1,59 @@
+# find the parentNode of a term, once parentNode of the term occurs, times of the parentNode +1
+# calculate the number of whole childnodes of the parent node
+# draw boxplots and count the mean
+
+
+from xml.dom.minidom import parse
+
+
+import numpy as np
+import xml.dom.minidom
+DOMTree = xml.dom.minidom.parse("go_obo.xml")
+collection = DOMTree.documentElement
+terms = collection.getElementsByTagName("term")
+print('the number of terms: ',len(terms))
+dic = {}
+list = []
+for term in terms:
+	ids = term.getElementsByTagName('id')[0]
+	dic.update({ids.childNodes[0].data: 0})
+
+for term in terms:
+	ids = term.getElementsByTagName('id')[0]
+	category = term.getElementsByTagName('is_a')
+	for everyis_a in category:
+		dic[everyis_a.childNodes[0].data]+=1
+	for everyis_a in category:
+		dic[everyis_a.childNodes[0].data] += dic[ids.childNodes[0].data]
+for term in terms:
+	ids = term.getElementsByTagName('id')[0]
+	list.append(dic[ids.childNodes[0].data])
+
+list_translation=[]
+for term in terms:
+	ids = term.getElementsByTagName('id')[0]
+	df = term.getElementsByTagName('def')[0]
+	defstr=df.getElementsByTagName('defstr')[0]
+	if 'translation' in defstr.childNodes[0].data:
+		list_translation.append(dic[ids.childNodes[0].data])
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(15,6))
+plt.title('Distribution of the gene ontology',fontsize=20)
+plt.ylabel('the number of childNodes')
+plt.boxplot(list, vert=True, whis=1, patch_artist=True, meanline=True, showbox= True, showcaps=True, showfliers=False,notch=False)
+plt.xticks([1],['overall gene ontology'],fontsize=10)
+plt.show()
+plt.figure(figsize=(15,6))
+plt.title('Distribution of the translation related genes',fontsize=20)
+plt.ylabel('the number of childNodes')
+plt.boxplot(list_translation, vert=True, whis=1, patch_artist=True, meanline=False, showbox= True, showcaps=True, showfliers=False,notch=False)
+plt.xticks([1],['translation related gene'],fontsize=10)
+plt.show()
+plt.xticks([1],['translation related genes'],fontsize=10)
+print('whole genes mean: ',np.mean(list))
+print('translation mean: ', np.mean(list_translation))
+if np.mean(list_translation)>=np.mean(list):
+	print('On average, translation related genes have more childeNodes.')
+else:
+	print('On average, the overall genes ontology has more childeNodes.')
