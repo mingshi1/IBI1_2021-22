cf4c52d180811b6f433647cf4177bffd57066927
mingshi1 <mingshi.21@intl.zju.edu.cn>
2022-05-24 21:09:25 +0800

    _

--- Practical8/fragment_counter.py
+++
@@ -1,9 +0,0 @@
-# input seq
-# find the number of 'GAATTC'
-# add 1 to the number of 'GAATTC'
-import re
-seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'
-Fn=len(re.findall(r'GAATTC',seq))+1   # Fn means the number of fragments
-print('The total number of fragments is ',Fn,' if we applied the EcoRI enzyme to it.')
-
-
